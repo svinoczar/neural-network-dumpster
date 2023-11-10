f1366758ab60ebb8a5ed9d7463104cbfe124ac2e
svinoczar <alexsupper75@gmail.com>
2023-11-11 02:43:59 +0300

    add consolidation tool in notebook

---
+++ utils/analytics/consolidation.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("/wsl.localhost/Debian/home/sunshine/crypto-prediction/datasets/binance-bitcoin.csv")
+print(df.head())
